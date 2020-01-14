be9d9c91557ff53e56667b194c9ef21fd2a84adf
Richard Link <34758591+VikoticRlink@users.noreply.github.com>
2020-01-14 12:50:48 -0800

    Add files via upload

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */ 
+public class Constants {
+	/**
+	 * Which PID slot to pull gains from. Starting 2018, you can choose from
+	 * 0,1,2 or 3. Only the first two (0,1) are visible in web-based
+	 * configuration.
+	 */
+	public static final int kSlotIdx = 0;
+
+	/**
+	 * Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
+	 * now we just want the primary one.
+	 */
+	public static final int kPIDLoopIdx = 0;
+
+	/**
+	 * Set to zero to skip waiting for confirmation, set to nonzero to wait and
+	 * report to DS if action fails.
+	 */
+	public static final int kTimeoutMs = 30;
+	
+	/* Choose so that Talon does not report sensor out of phase */
+	public static boolean kSensorPhase = true;
+
+	/**
+	 * Choose based on what direction you want to be positive,
+	 * this does not affect motor invert. 
+	 */
+	public static boolean kMotorInvert = false;
+
+	/**
+	 * Gains used in Positon Closed Loop, to be adjusted accordingly
+     * Gains(kp, ki, kd, kf, izone, peak output);
+     */
+
+
+	 //http://www.ni.com/en-us/innovations/white-papers/06/pid-theory-explained.html
+	 
+	static final Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+	static final Gains kElevatorGains = new Gains(0.08, 0.0, 1.0, 0.0, 0, 1.0);
+	static final Gains kWormGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+	static final Gains kClimbDriveGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    //--- Pneumatics ---//
+    public static Compressor airSupply;
+    public static DoubleSolenoid Valve1;
+    public static DoubleSolenoid Valve2;
+
+    //--- LED ---//
+    public static AddressableLED m_led;
+    public static AddressableLEDBuffer m_ledBuffer;
+    public static int m_rainbowFirstPixelHue;
+
+    //-- Color Sensor ---//
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    Color detectedColor = m_colorSensor.getColor();
+
+public static void init() {
+    //--- Pneumatics ---//
+    airSupply = new Compressor(60);
+    Valve1 = new DoubleSolenoid(60, 0, 1);
+    Valve2 = new DoubleSolenoid(60, 2, 3);
+    //airSupply.start();
+
+    //--- LEDs ---//
+    m_led = new AddressableLED(1);
+    m_ledBuffer = new AddressableLEDBuffer(12);
+    m_led.setLength(m_ledBuffer.getLength());
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+
+}
+//--- LED Functions ---//
+  public static void LEDRainbow(){
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+        final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+        m_ledBuffer.setHSV(i, hue, 255, 128);
+      }
+      m_rainbowFirstPixelHue += 3;
+      m_rainbowFirstPixelHue %= 180;
+    m_led.setData(m_ledBuffer);
+  }
+    public static void LEDColor(int Red, int Green, int Blue){
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            m_ledBuffer.setRGB(i, Red, Green, Blue);
+        }
+    }
+    public static void LEDMatchSensor(){
+        //for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+        //    m_ledBuffer.setRGB(i, (int) detectedColor.red, (int) detectedColor.green, (int) detectedColor.blue);
+        //}
+        //m_led.setData(m_ledBuffer);
+    }
+}
